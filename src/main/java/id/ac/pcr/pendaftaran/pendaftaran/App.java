57d91229de05257e0cc28f332bfa313b2dc1660a
Satria Perdana <Satria Perdana@satria-4b68a5aa>
2013-04-02 09:16:29 +0700

    percobaan 1

---
+++ src/main/java/id/ac/pcr/pendaftaran/pendaftaran/App.java
@@ -0,0 +1,13 @@
+package id.ac.pcr.pendaftaran.pendaftaran;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
